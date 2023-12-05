7021ede4d19b0d8b952f17d151f7c982e16942b0
Mrdjvj <97501701+Mrdjvj@users.noreply.github.com>
2023-12-05 10:01:02 +0530

    Add files via upload

---
+++ Career-Guidance-System/admin/appointments.py
@@ -0,0 +1,63 @@
+import mysql.connector
+
+
+def create_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="vedant2803",
+        database="DBMS_miniproject",
+        auth_plugin="mysql_native_password",
+    )
+    return connection
+
+
+def get_appointments_data():
+    connection = create_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("DROP PROCEDURE IF EXISTS GetAppointmentData")
+
+    query = """
+CREATE PROCEDURE GetAppointmentData()
+BEGIN
+    SELECT
+        A.Appointment_ID,
+        A.Student_ID,
+        U.Student_Name,
+        U.Email AS Student_Email,
+        U.Mobile AS Student_Mobile,
+        A.Counsellor_ID,
+        C.Counsellor_Name,
+        C.Email AS Counsellor_Email,
+        C.Mobile AS Counsellor_Mobile,
+        A.Date,
+        A.Start_Time,
+        C.Fees,
+        COUNT(A.Appointment_ID) AS TotalAppointmentsForStudent,
+        (
+            SELECT COUNT(Appointment_ID)
+            FROM Appointments
+            WHERE Counsellor_ID = A.Counsellor_ID
+        ) AS TotalAppointmentsForCounselor
+    FROM Appointments A
+    INNER JOIN Users U ON A.Student_ID = U.Student_ID
+    INNER JOIN Counsellor C ON A.Counsellor_ID = C.Counsellor_ID
+    GROUP BY A.Appointment_ID, A.Student_ID, U.Student_Name, U.Email, U.Mobile,
+             A.Counsellor_ID, C.Counsellor_Name, C.Email, C.Mobile,
+             A.Date, A.Start_Time, C.Fees;
+END
+
+
+    """
+
+    cursor.execute(query)
+    cursor.callproc("GetAppointmentData")
+
+    for result in cursor.stored_results():
+        appointments_data = result.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return appointments_data
---
+++ DBMS_miniproject.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE DBMS_miniproject;
+USE DBMS_miniproject;
+
+
+INSERT INTO career_options(Career_ID,Job_Title,Education,Description,Industries,Salary) VALUES(1,'Software Developer',"Bachelor's in Computer Science",'Develop and maintain software applications','Technology',80000.00);
+INSERT INTO career_options(Career_ID,Job_Title,Education,Description,Industries,Salary) VALUES(2,'Marketing Manager',"Bachelor's in Marketing",'Plan and execute marketing strategies','Marketing',75000.00);
+INSERT INTO career_options(Career_ID,Job_Title,Education,Description,Industries,Salary) VALUES(3,'Registered Nurse',"Bachelor's in Nursing","Provide medical care and support to patients.",'Healthcare',60000.00);
+INSERT INTO career_options(Career_ID,Job_Title,Education,Description,Industries,Salary) VALUES(4,'Financial Analyst',"Bachelor's in Finance ",'Analyze financial data and provide insights.','Finance',90000.00);
+INSERT INTO career_options(Career_ID,Job_Title,Education,Description,Industries,Salary) VALUES(5,'Teacher',"Bachelor's in Education",'Educate and inspire students.','Education',50000.00);
+
+
+INSERT INTO college(College_ID,College_Name,Website,Ranking,Courses,Entrance_Exams,Type) VALUES(1,'RV College',"http://www.rvcollege.com",10,'Computer Science, Engineering','Entrance1, Entrance2','undergraduate');
+INSERT INTO college(College_ID,College_Name,Website,Ranking,Courses,Entrance_Exams,Type) VALUES(2,'PES University',"http://www.pesuniversity.com",5,'Mathematics, Physics, Chemistry','Entrance1, Entrance2','undergraduate');
+INSERT INTO college(College_ID,College_Name,Website,Ranking,Courses,Entrance_Exams,Type) VALUES(3,'IIIT ',"http://www.iiitinstitute.com",15,'Business Administration, Economics','Entrance','postgraduate');
+INSERT INTO college(College_ID,College_Name,Website,Ranking,Courses,Entrance_Exams,Type) VALUES(4,'PQR Institute',"http://www.pqrinstitute.com",8,'Medical Science, Nursing','Entrance','undergraduate');
+INSERT INTO college(College_ID,College_Name,Website,Ranking,Courses,Entrance_Exams,Type) VALUES(5,'LMN College',"'http://www.lmncollege.com",20,'Arts, Literature','Entrance','undergraduate');
+INSERT INTO college(College_ID,College_Name,Website,Ranking,Courses,Entrance_Exams,Type) VALUES(6,'GHI University',"http://www.ghiuniversity.com",3,'Physics, Chemistry, Biology','Entrance','postgraduate');
